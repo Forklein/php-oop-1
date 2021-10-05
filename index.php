edeaf7a029d5324f4d75c2daecef4e5945c22af0
Forklein <giuseppepisani2@gmail.com>
2021-10-05 15:02:49 +0200

    Print first instance

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
+<?php
+require_once __DIR__ . '/models/Movie.php'
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -9,6 +12,17 @@
 </head>
 
 <body>
+
+
+    <?php $movie_1 = new Movie(1, 'La vita è bella', 'Drammatico', 1997, '110 min') ?>
+    <div class="movie">
+        <h1><?= $movie_1->name ?></h1>
+        <h2><?= $movie_1->genre ?></h2>
+        <p><?= $movie_1->year ?></p>
+        <p><?= $movie_1->duration ?></p>
+    </div>
+
+
 
 </body>
 
--- models/Movie.php
+++ models/Movie.php
@@ -1,9 +1,27 @@
 <?php
 
+/**
+ * Info
+ * @param int $id
+ * @param string $name 
+ * @param string $genre
+ * @param int $year
+ * @param string $duration
+ */
+
 class Movie
 {
+    public $id;
     public $name;
     public $genre;
     public $year;
     public $duration;
+    public function __construct($id, $name, $genre, $year, $duration)
+    {
+        $this->id = $id;
+        $this->name = $name;
+        $this->genre = $genre;
+        $this->year = $year;
+        $this->duration = $duration;
+    }
 }
